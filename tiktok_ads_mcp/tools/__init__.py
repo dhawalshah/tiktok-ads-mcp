d444b46e5b2dc1969a75c0777ac45408021f017d
Dhawal <dhawaljshah@gmail.com>
2026-04-09 13:11:48 +0800

    feat: add get_advertiser_info tool (advertiser account metadata)

--- tiktok_ads_mcp/tools/__init__.py
+++ tiktok_ads_mcp/tools/__init__.py
@@ -2,6 +2,7 @@
 
 from .get_business_centers import get_business_centers
 from .get_authorized_ad_accounts import get_authorized_ad_accounts
+from .get_advertiser_info import get_advertiser_info
 from .get_campaigns import get_campaigns
 from .get_ad_groups import get_ad_groups
 from .get_ads import get_ads
@@ -9,7 +10,8 @@ from .reports import get_reports
 
 __all__ = [
     "get_business_centers",
-    "get_authorized_ad_accounts", 
+    "get_authorized_ad_accounts",
+    "get_advertiser_info",
     "get_campaigns",
     "get_ad_groups",
     "get_ads",
